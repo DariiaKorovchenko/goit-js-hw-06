d3edb5118790cdc9615df020e099662a06986e49
DariiaKorovchenko <samurais.daughter007@gmail.com>
2023-06-12 21:35:56 +0300

    Done task-01

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const categoriesEl = document.querySelectorAll('li.item');
+const numberOfCategories = categoriesEl.length;
+console.log('Number of categories: ', numberOfCategories);
+
+let numberOfItems = 0;
+categoriesEl.forEach(element => {
+  console.log('Category: ', element.firstElementChild.innerHTML);
+  numberOfItems = element.children[1].children.length;
+  console.log('Elements: ', numberOfItems);
+});
