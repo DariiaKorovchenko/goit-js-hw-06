3fc900f02568154f5fc670bfc0b307dfb47736d6
DariiaKorovchenko <samurais.daughter007@gmail.com>
2023-06-14 12:32:56 +0300

    Done Task 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const ingredientsItemsEl = ingredients.map(ingredient => {
+  const newItem = document.createElement('li');
+  newItem.textContent = ingredient;
+  newItem.classList.add('item');
+
+  return newItem;
+});
+
+ingredientsEl.append(...ingredientsItemsEl);
