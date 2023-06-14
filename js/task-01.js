268a59f1a792125c73fb386624d1bbcc9be03af8
DariiaKorovchenko <samurais.daughter007@gmail.com>
2023-06-14 14:19:19 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -4,7 +4,7 @@ console.log('Number of categories: ', numberOfCategories);
 
 let numberOfItems = 0;
 categoriesEl.forEach(element => {
-  console.log('Category: ', element.firstElementChild.innerHTML);
+  console.log('Category: ', element.firstElementChild.textContent);
   numberOfItems = element.children[1].children.length;
   console.log('Elements: ', numberOfItems);
 });
