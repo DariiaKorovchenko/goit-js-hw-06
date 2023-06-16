8b9b07b0e6c42d3d2d221813be98b7eb7cdfe83b
DariiaKorovchenko <samurais.daughter007@gmail.com>
2023-06-16 16:12:12 +0300

    Done Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnEl = document.querySelector(".change-color");
+const textEl = document.querySelector(".color");
+const body = document.querySelector("body");
+
+btnEl.addEventListener("click", onChangeColor);
+
+function onChangeColor(event) {
+  const randomColor = getRandomHexColor();
+  textEl.textContent = randomColor;
+  body.style.backgroundColor = randomColor;
+}
