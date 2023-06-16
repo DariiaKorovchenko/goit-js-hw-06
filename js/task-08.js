cec53bdb61449ce3a176f36299518ed02d543cbd
DariiaKorovchenko <samurais.daughter007@gmail.com>
2023-06-16 16:01:50 +0300

    Done Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -2,4 +2,19 @@ const formEl = document.querySelector(".login-form");
 
 formEl.addEventListener("submit", onFormSubmit);
 
-function onFormSubmit(event) {}
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (
+    event.currentTarget.elements.email.value === "" ||
+    event.currentTarget.elements.password.value === ""
+  ) {
+    return window.alert("All form fields must be completed");
+  }
+  const formData = {
+    email: event.currentTarget.elements.email.value,
+    password: event.currentTarget.elements.password.value,
+  };
+
+  console.log(formData);
+  formEl.reset();
+}
